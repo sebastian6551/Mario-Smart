e7e3081690e591c01a1e9f7e41a3a4f35c18738b
JuanCamilo-R <juan.randazzo@correounivalle.edu.co>
2022-10-26 18:49:29 -0500

    Refactoring code

--- main.py
+++ main.py
@@ -1,72 +1,49 @@
-import sys, pygame
 import numpy as np
-import node
+from node import Node
 
-with open('matriz.txt','r') as f:
-    datos = ''.join(f.readlines()).replace('\n',';')
+with open('matriz.txt', 'r') as f:
+    board_txt = ''.join(f.readlines()).replace('\n', ';')
 
-m = np.matrix(datos)
-
-nodoActual = node.Nodo(m,None," ",0,0)
-arr2 = nodoActual.buscarMario()
-arr= []
-arr.append(nodoActual)
+world = np.matrix(board_txt)
 
 
+currentNode = Node(world, None, " ", 0, 0)
+marioPos = currentNode.searchForMario()
+stack = []
+stack.append(currentNode)
 
-while(not (arr[0].esmeta(arr2[0], arr2[1]))):
- if(not(arr2[1]+1 >9) and arr[0].verEstado()[arr2[0],arr2[1]+1] != 1 ): #ver si lado derecho esta sin obstaculos
-    #print("si1")
-    hijo = node.Nodo(arr[0].verEstado(), arr[0].verEstado(), "derecha", arr[0].profundidad +1 , arr[0].costo +1)
-    hijo.moverDer(arr2[0], arr2[1])
-    arr.append(hijo)
 
- if(not(arr2[1]-1 <0) and arr[0].verEstado()[arr2[0],arr2[1]-1] != 1): #ver si lado izquiero esta sin obstaculos
-    #print("si2")
-    hijo = node.Nodo(arr[0].verEstado(), arr[0].verEstado(), "izquierda", arr[0].profundidad +1 , arr[0].costo +1)
-    hijo.moverIz(arr2[0], arr2[1])
-    arr.append(hijo)
+while (not (stack[0].isGoal(marioPos[0], marioPos[1]))):
+    # Check if right side is free
+    if (not (marioPos[1]+1 > 9) and stack[0].getState()[marioPos[0], marioPos[1]+1] != 1):
+        son = Node(stack[0].getState(), stack[0],
+                   "right", stack[0].getDepth() + 1, stack[0].getCost() + 1)
+        son.moveRight(marioPos)
+        stack.append(son)
 
- if(not(arr2[0]+1>9) and arr[0].verEstado()[arr2[0]+1,arr2[1]] != 1 ): #ver si lado abajo esta sin obstaculos
-      #print("si3")
-      hijo = node.Nodo(arr[0].verEstado(), arr[0].verEstado(), "abajo", arr[0].profundidad +1 , arr[0].costo +1)
-      hijo.moverAb(arr2[0], arr2[1])
-      arr.append(hijo)
+    # Check if left side is free
+    if (not (marioPos[1]-1 < 0) and stack[0].getState()[marioPos[0], marioPos[1]-1] != 1):
+        son = Node(stack[0].getState(), stack[0],
+                   "left", stack[0].getDepth() + 1, stack[0].getCost() + 1)
+        son.moveLeft(marioPos)
+        stack.append(son)
 
- if( not(arr2[0]-1<0) and arr[0].verEstado()[arr2[0]-1,arr2[1]] != 1 ): #ver si lado arriba esta sin obstaculos
-      #print("si4")
-      hijo = node.Nodo(arr[0].verEstado(), arr[0].verEstado(), "arriba", arr[0].profundidad +1 , arr[0].costo +1)
-      hijo.moverAr(arr2[0], arr2[1])
-      arr.append(hijo)
- arr.pop(0)
- nodoActual1 = arr[0] 
- arr2 = nodoActual1.buscarMario()
- 
-print(arr[0]._padre)
-print(arr[0].verEstado())
+    # Check if down side is free
+    if (not (marioPos[0]+1 > 9) and stack[0].getState()[marioPos[0]+1, marioPos[1]] != 1):
+        son = Node(stack[0].getState(), stack[0],
+                   "down", stack[0].getDepth() + 1, stack[0].getCost() + 1)
+        son.moveDown(marioPos)
+        stack.append(son)
 
-""""
-pygame.init()
+    # Check if up side is free
+    if (not (marioPos[0]-1 < 0) and stack[0].getState()[marioPos[0]-1, marioPos[1]] != 1):
+        son = Node(stack[0].getState(), stack[0],
+                   "up", stack[0].getDepth() + 1, stack[0].getCost() + 1)
+        son.moveUp(marioPos)
+        stack.append(son)
+    stack.pop(0)
+    newNode = stack[0]
+    marioPos = newNode.searchForMario()
 
-size = width, height = 600, 600
-speed = [2, 2]
-black = 0, 0, 0 
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("mario.png")
-ballrect = ball.get_rect()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()"""
+print(stack[0].getFather().getDepth())
+print(stack[0].getState())
--- node.py
+++ node.py
@@ -1,67 +1,110 @@
-class Nodo:
-     
- def __init__(self,estado, padre,operador, profundidad, costo):
-  self._estado=estado;
-  self._padre=padre;
-  self.operador=operador;
-  self.profundidad=profundidad;
-  self.costo=costo;
+class Node:
 
- def verEstado(self):
-    return self._estado.copy()
+    EMPTY = 0
+    BLOCK = 1
+    MARIO = 2
+    STAR = 3
+    KOOPA = 5
+    FLOWER = 4
 
- def moverDer(self,ubiMariox,ubiMarioy):
-   self._estado[ubiMariox,ubiMarioy]=0
-   self._estado[ubiMariox,ubiMarioy+1]=2
-   return self;
+    def __init__(self, state, father, operator, depth, cost):
+        self.__state = state
+        self.__father = father
+        self.__operator = operator
+        self.__depth = depth
+        self.__cost = cost
+        self.__marioPos = []
 
- def moverIz(self,ubiMariox,ubiMarioy):
-   self._estado[ubiMariox,ubiMarioy]=0
-   self._estado[ubiMariox,ubiMarioy-1]=2
-   return self;
-   
+    def getState(self):
+        return self.__state
 
- def moverAb(self,ubiMariox,ubiMarioy):
-   self._estado[ubiMariox,ubiMarioy]=0
-   self._estado[ubiMariox+1,ubiMarioy]=2
-   return self;
+    def getFather(self):
+        return self.__father
 
- def moverAr(self,ubiMariox,ubiMarioy):
-   self._estado[ubiMariox,ubiMarioy]=0
-   self._estado[ubiMariox-1,ubiMarioy]=2
-   return self;
+    def getOperator(self):
+        return self.__operator
 
- def profundidad(self):
-     print("La profundidad del nodo es " + self.profundidad);
-  
+    def getDepth(self):
+        return self.__depth
 
- def operador(self):
-     print("El operador usado para llegar a este nodo fue " + self.operador);
-  
- def buscarMario(self):
-    arr = []
-    arr3 = self._estado
-    for i in range (10):
-        for j in range (10):
-            if(arr3[i,j]==2):
-                arr.append(i)
-                arr.append(j)
-                return arr
- def esmeta(self,i,j):
-    if(j+1<9):
-        if(self._estado[i,j+1]==6):
-            return True
-    if(i-1>0):
-        if(self._estado[i-1,j]==6):
-            return True
-    if(i+1<9):
-        if(self._estado[i+1,j]==6):
-            return True
-    if(j-1):
-        if(self._estado[i,j-1]==6):
-            return True
-    #if(self._estado[i,j+1]==6 or self._estado[i-1,j]==6 or self._estado[i+1,j]==6 or self._estado[i,j-1]==6):
-    #    return True
-    else:
-        return False
+    def getCost(self):
+        return self.__cost
 
+    def getMarioPos(self):
+        return self.__marioPos
+
+    def setState(self, newState):
+        self.__state = newState
+
+    def setFather(self, newFather):
+        self.__father = newFather
+
+    def setOperator(self, newOperator):
+        self.__operator = newOperator
+
+    def setDepth(self, newDepth):
+        self.__depth = newDepth
+
+    def setCost(self, newCost):
+        self.__cost = newCost
+
+    def setMarioPos(self, newMarioPos):
+        self.__marioPos = newMarioPos
+
+    def getState(self):
+        return self.__state.copy()
+
+    def moveRight(self, posMario):
+        self.__state[posMario[0], posMario[1]] = 0
+        self.__state[posMario[0], posMario[1]+1] = 2
+        return self
+
+    def moveLeft(self, posMario):
+        self.__state[posMario[0], posMario[1]] = 0
+        self.__state[posMario[0], posMario[1]-1] = 2
+        return self
+
+    def moveDown(self, posMario):
+        self.__state[posMario[0], posMario[1]] = 0
+        self.__state[posMario[0]+1, posMario[1]] = 2
+        return self
+
+    def moveUp(self, posMario):
+        self.__state[posMario[0], posMario[1]] = 0
+        self.__state[posMario[0]-1, posMario[1]] = 2
+        return self
+
+    def showDepth(self):
+        print("The node's depth is: " + str(self.profundidad))
+
+    def showOperator(self):
+        print("The operator used to get to this node was: " + self.operador)
+
+    def searchForMario(self):
+        marioPos = []  # Mario position [x,y]
+        arr3 = self.__state
+        for i in range(10):
+            for j in range(10):
+                if (arr3[i, j] == 2):
+                    marioPos.append(i)
+                    marioPos.append(j)
+        self.setMarioPos(marioPos)
+        return marioPos
+
+    def isGoal(self, i, j):
+        if (j+1 < 9):
+            if (self.__state[i, j+1] == 6):
+                return True
+        if (i-1 > 0):
+            if (self.__state[i-1, j] == 6):
+                return True
+        if (i+1 < 9):
+            if (self.__state[i+1, j] == 6):
+                return True
+        if (j-1 > 0):
+            if (self.__state[i, j-1] == 6):
+                return True
+            # if(self.state[i,j+1]==6 or self.state[i-1,j]==6 or self.state[i+1,j]==6 or self.state[i,j-1]==6):
+            #    return True
+        else:
+            return False
